7f8b098f73eebca86c5daa35d53d1aaa49d879f8
zed13 <ddanilchuk13@gmail.com>
2025-08-12 10:50:50 +0200

    --verbose option was added

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
        print("You should run it as: uv run main.py <prompt>")
-       exit(0)
+       exit(1)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+    verbose = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=sys.argv[1],
+        contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    print("Hello from ai-agent!")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
