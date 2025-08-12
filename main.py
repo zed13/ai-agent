8ac244fed12a1314f81d376550ce2edf9ef1b943
zed13 <ddanilchuk13@gmail.com>
2025-08-12 10:28:04 +0200

    Prompt setting via args

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
 def main():
+    if len(sys.argv) == 0:
+       print("You should run it as: uv run main.py <prompt>")
+       exit(0)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=sys.argv[1],
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Hello from ai-agent!")
 
 
